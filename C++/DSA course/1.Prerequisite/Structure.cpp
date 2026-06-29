a81470f288c3f0857375ea09fb9b22a88cce93aa
Debojyoti Deb <debjoy040@gmail.com>
2026-06-30 00:52:19 +0530

    Structure Introduction

---
+++ C++/DSA course/1.Prerequisite/Structure.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct Rectangle
+{
+    int length;
+    int breadth;
+};
+
+
+int main(){
+
+    struct Rectangle r1 = {10,5};
+    printf( "%d" , sizeof(r1));
+
+}
