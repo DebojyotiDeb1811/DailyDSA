21115e73937494b432860319d00399e3b294f52f
Debojyoti Deb <debjoy040@gmail.com>
2026-07-04 21:07:13 +0530

    Sum By Recursion

---
+++ C++/DSA course/2.Recursion/SumByRecursion.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std;
+
+int Sum(int n){
+
+    if( n ==  0 ){
+      return 0;
+    }
+
+    else{
+     return Sum(n-1) + n;
+    }
+}
+
+int main(){
+    int r;
+    r = Sum(10);
+    cout << r;
+    return 0;
+}
